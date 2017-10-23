d47475744b15e79407062a95dc6f3e48917ce132
Bruledamien <Damien@MacBook-de-Damien.local>
2017-10-23 22:54:08 +0200

    version dipdip

--- dynamicPatternsD3.js
+++ dynamicPatternsD3.js
@@ -1,29 +1,49 @@
-var dynamicPattern = function() {
+// PARAMS : définition des couleurs
+var colors = {
+  ultraMarine: "rgba(19,37,60,0.8)",
+  vert: "rgba(56,78,60, 0.8)",
+  rosePoudre: "rgba(243,218,206,0.8)",
+  justeBleu: "rgba(13,29,236,0.8)",
+  corail: "rgba(241,88,65,0.8)",
+  soleilHiver: "rgba(255,248,160,0.8)"
+};
 
-  this.nbSites = 50
+var backgroundColors = {
+  blanc: "rgba(255,255,255,0.8)",
+  gris: "rgba(205,205,205,0.8)",
+  noir: "rgba(0,0,0,0.8)"
+}
 
-  var ultraMarine = "rgba(19,37,60,1)";
-  var vert = "rgba(56,78,60, 0.5)";
-  var rosePoudre = "rgba(243,218,206,1)";
-  var justeBleu = "rgba(13,29,236,0.8)";
-  var corail = "rgba(241,88,65,0.6)";
-  var soleilHiver = "rgba(255,248,160,1)";
 
-  var svg = d3.select("svg"),
-      width = +svg.attr("width"),
-      height = +svg.attr("height");
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-  // SITES
 
-  var sites = d3.range(this.nbSites)
-      .map(function(d) { return [Math.random() * width, Math.random() * height]; });
+// SITES
+var calc = function(val) {
+  return Math.sqrt((1.001 - val/100)) * Math.sqrt(width * height);
+}
+
+var minZones = 1;
+var maxZones = 100;
+var sitesAll = d3.range(calc(minZones)).map(
+  function(d) { return [Math.random() * width, Math.random() * height]; }
+);
+
+var sites = sitesAll.slice(0, this.tailleZones);
+
+var dynamicPattern = function(sites) {
 
   var site = svg.append("g")
       .attr("class", "sites")
     .selectAll("circle")
     .data(sites)
-    .enter().append("circle")
+
+  site.enter().append("circle")
       .call(redrawSite);
+
+  site.exit().remove()
 
   function redrawSite(site) {
     site
@@ -42,7 +62,10 @@ var dynamicPattern = function() {
       .attr("class", "polygons")
     .selectAll("path")
     .data(polygonData)
-    .enter().append("path")
+
+  polygon.exit().remove()
+
+  polygonEnter = polygon.enter().append("path")
       .call(redrawPolygon);
 
   function redrawPolygon(polygon) {
@@ -57,7 +80,9 @@ var dynamicPattern = function() {
 
   var frontData = [], backData =[];
 
-  computeStitchData(polygon);
+  computeStitchData(polygonEnter);
+
+  d3.select("g.stitchb").remove()
 
   var stitchb = svg.append("g")
       .attr("class", "stitchb")
@@ -65,6 +90,8 @@ var dynamicPattern = function() {
     .data(backData)
     .enter().append("line")
       .call(redrawLine);
+
+  d3.select("g.stitchf").remove()
 
   var stitchf = svg.append("g")
       .attr("class", "stitchf")
@@ -79,7 +106,6 @@ var dynamicPattern = function() {
         .attr("x2", function(d) { return d[2]; })
         .attr("y2", function(d) { return d[3]; });
   }
-
 
   // STITCH COMPUTATION
 
@@ -150,7 +176,7 @@ var dynamicPattern = function() {
 
   function randomDensity() {
     // PARAMS : densité
-    densities = [0.00001, 0.00001, 0.00001, 0.02, 0.05, 0.1, 0.1, 0.125]
+    densities = [0.00001, 0.00001, 0.00001, 0.00001, 0.02, 0.02, 0.05, 0.1, 0.1, 0.125]
     return densities[Math.floor(Math.random() * densities.length)];
   }
 
@@ -159,11 +185,44 @@ var dynamicPattern = function() {
   }
 }
 
+var paramsObj = function() {
+  // PARAMS: valeurs par défaut
+  this.tailleZones = 5;
+  this.filRecto = colors["rosePoudre"];
+  this.filVerso = colors["justeBleu"];
+  this.backGround = backgroundColors["noir"];
+}
+
 window.onload = function() {
-  var pattern = new dynamicPattern();
+
+  dynamicPattern(sites);
+  var params = new paramsObj();
   var gui = new dat.GUI();
-  var controller = gui.add(pattern, 'nbSites', 0, 200);
-  controller.onChange(function(value) {
-    dynamicPattern()
-  })
+  var sitesCtrl = gui.add(params, 'tailleZones', minZones, maxZones);
+  var rectoCtrl = gui.add(params, 'filRecto', colors);
+  var versoCtrl = gui.add(params, 'filVerso', colors);
+  var backgroundCtrl = gui.add(params, 'backGround', backgroundColors);
+
+
+  sitesCtrl.onChange(function(value) {
+    var sites = sitesAll.slice(0, calc(value));
+    dynamicPattern(sites);
+  });
+
+  rectoCtrl.onChange(function(value) {
+    d3.select("g.stitchf")
+      .selectAll("line")
+      .attr('stroke', function(d) { return value; });
+  });
+  versoCtrl.onChange(function(value) {
+    d3.select("g.stitchb")
+      .selectAll("line")
+      .attr('stroke', function(d) { return value; });
+  });
+  backgroundCtrl.onChange(function(value) {
+    d3.select("svg")
+      .attr("style", function(d) {
+        return "background: " + value;
+      });
+  });
 };
